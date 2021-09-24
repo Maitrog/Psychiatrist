a3dc60353fb1d8f4b2e91ad28171991d2b188444
Maitrog <35650757+Maitrog@users.noreply.github.com>
2021-09-24 17:25:30 +0300

    Add pacient, body, hair, face scripts

---
+++ Assets/Scripts/Face.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : MonoBehaviour
+{
+    public Transform top;
+    public Transform bottom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
