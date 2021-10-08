fc77be65a459d8eef59c54e4606c82720a1936f4
Maitrog <35650757+Maitrog@users.noreply.github.com>
2021-10-08 17:45:23 +0300

    Add collection of current target patients

---
+++ Assets/Scripts/CurrentTargetsSO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Current Targets", menuName = "CurrentTargets")]
+public class CurrentTargetsSO : ScriptableObject
+{
+    public List<PatientSO> currentTarget;
+    public List<bool> isReady = new List<bool> { true, true, true, true };
+    public int count;
+
+    public void AddPatient(int index, Patient patient, GameObject newHair, GameObject newFace, GameObject newBody)
+    {
+        if (isReady[index])
+        {
+            currentTarget[index].BecomeCurrent(patient, newHair, newFace, newBody);
+            isReady[index] = false;
+            count++;
+        }
+    }
+
+    public void DeletePatient(int index)
+    {
+        if (index < 4)
+        {
+            isReady[index] = true;
+            count--;
+        }
+    }
+}
+
