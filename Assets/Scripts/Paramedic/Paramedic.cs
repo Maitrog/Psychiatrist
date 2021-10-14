92a516db520d0257cfcfb5a41c06da88e033f8cc
Maitrog <35650757+Maitrog@users.noreply.github.com>
2021-10-14 19:07:22 +0300

    Add paramedic

---
+++ Assets/Scripts/Paramedic/Paramedic.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paramedic : MonoBehaviour
+{
+    public Photo photo;
+
+    public Sex Sex;
+    public string Name;
+    public string Surname;
+    public string Patronymic;
+    public double Strength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
