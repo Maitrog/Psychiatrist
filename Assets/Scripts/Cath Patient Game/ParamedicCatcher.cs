26420535e6ef54b7da1b10ee8b255a402933c8b8
Maitrog <35650757+Maitrog@users.noreply.github.com>
2021-10-22 20:06:52 +0300

    Add joystick

--- Assets/Scripts/Cath Patient Game/ParamedicCatcher.cs
+++ Assets/Scripts/Cath Patient Game/ParamedicCatcher.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+    using UnityEngine;
 
 public class ParamedicCatcher : MonoBehaviour
 {   
@@ -6,30 +6,39 @@ public class ParamedicCatcher : MonoBehaviour
 
     public Movement movement { get; private set; }
 
+    public Joystick joystick;
+
     private void Awake()
     {
         movement = GetComponent<Movement>();
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || joystick.Vertical >= .2f)
         {
             movement.SetDirection(Vector2.up);
         }
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || joystick.Vertical <= -.2f)
         {
             movement.SetDirection(Vector2.down);
         }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) || joystick.Horizontal <= -.2f)
         {
             movement.SetDirection(Vector2.left);
         }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) || joystick.Horizontal >= .2f)
         {
             movement.SetDirection(Vector2.right);
         }
 
         float angle = Mathf.Atan2(movement.direction.x, movement.direction.y);
         transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+    }
+
+    public void ResetState()
+    {
+        gameObject.SetActive(true);
+        movement.ResetState();
     }
 }
