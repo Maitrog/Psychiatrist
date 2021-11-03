d1a580b4ea42d950083c12fb75e6003fd700b322
Maitrog <35650757+Maitrog@users.noreply.github.com>
2021-11-03 15:55:04 +0300

    Add game result

--- Assets/Scripts/Cath Patient Game/GameManager.cs
+++ Assets/Scripts/Cath Patient Game/GameManager.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +14,11 @@ public class GameManager : MonoBehaviour
     public PatientObject currentPatient;
     public ParamedicObject currentParamedic;
 
+    public GameObject resultPanel;
+    public GameObject gridPanel;
+
+    private Color green = new Color(27, 147, 32);
+    private Color red = new Color(157, 24, 27);
     public void Start()
     {
         NewGame();
@@ -23,7 +31,7 @@ public class GameManager : MonoBehaviour
     }
     private void NewGame()
     {
-        float spM = 1.0f - (float)(currentParamedic.Speed - currentPatient.Speed) / 16.0f;
+        float spM = 1.0f - (float)(currentParamedic.Speed + 1 - currentPatient.Speed) / 16.0f;
         patient.speedMultiplaier = spM;
         patient.GetComponent<Movement>().speedMultiplaier = spM;
         paramedic.gameObject.SetActive(true);
@@ -38,8 +46,48 @@ public class GameManager : MonoBehaviour
 
     private void GameOver()
     {
-        Debug.Log("Game Over");
+        bool isWin = false;
+        HashSet<DiseaseType> posidivDisease = new HashSet<DiseaseType>();
+        HashSet<DiseaseType> negativeDisease = new HashSet<DiseaseType>();
+
+        foreach (Skill skill in currentParamedic.skills)
+        {
+            if (skill.Level == SkillLevel.LOWEST)
+            {
+                foreach (DiseaseType disease in skill.Diseases)
+                    negativeDisease.Add(disease);
+            }
+            else
+            {
+                foreach (DiseaseType disease in skill.Diseases)
+                    posidivDisease.Add(disease);
+            }
+        }
+        negativeDisease = new HashSet<DiseaseType>(negativeDisease.Intersect(posidivDisease).ToList());
+        posidivDisease = new HashSet<DiseaseType>(posidivDisease.Except(negativeDisease).ToList());
+        foreach (DiseaseType disease in currentPatient.Diseases)
+        {
+            if (posidivDisease.Contains(disease))
+                isWin = true;
+        }
+
         paramedic.gameObject.SetActive(false);
         patient.gameObject.SetActive(false);
+
+        Image background = resultPanel.GetComponent<Image>();
+        TextMeshProUGUI text = resultPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        if (isWin)
+        {
+            background.color = Color.green;
+            text.text = "YOU'R WIN";
+        }
+        else
+        {
+            background.color = Color.red;
+            text.text = "YOU'R LOOOOOOOOOOOOOOOOOOOOOOOSE";
+        }
+        gridPanel.SetActive(false);
+        resultPanel.SetActive(true);
+        Debug.Log("Game Over");
     }
 }
